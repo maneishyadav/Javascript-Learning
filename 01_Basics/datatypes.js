9a1258c7dc9dbb2bbb795830389219b122ca6d93
Maneish Yadav <155384531+maneishyadav@users.noreply.github.com>
2024-01-02 17:33:58 +0000

    Data Types

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+/*
+
+Number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol=> unique
+
+*/
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
