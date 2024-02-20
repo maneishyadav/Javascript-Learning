4117a70ebeed0c415f1439a8fe69e1cec07a1f1d
Maneish Yadav <155384531+maneishyadav@users.noreply.github.com>
2024-02-20 07:55:44 +0000

    conversion-operations

---
+++ 01_Basics/con-operation.js
@@ -0,0 +1,34 @@
+
+
+let score = "33abh"
+
+//console.log(score)
+//console.log(typeof(score))
+
+let convert = Number(score)
+
+//console.log(convert)
+
+// when the value changed in the number 
+// like:-  
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+
+
+let IsloggedIn = 1
+
+let BooleanIsLoogedIn = Boolean(IsloggedIn)
+console.log(BooleanIsLoogedIn)
+
+
+// when changed the calue from boolean like:-
+
+// 1 => true
+// 0 => false
+// "" => empty string value is 0
+// "manish" => true
+
+
